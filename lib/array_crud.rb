d9487e2b22bd5c54873d616ae07228816c489493
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-07 00:36:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  my_array[]
 end
 
 def update_element_from_index(array, index_number, element)
